7923ab519fafab0d14ee58dc8833fc37da3935ef
Ricardo Bocchi <ricardo@mobilemind.com.br>
2023-07-29 23:21:52 -0300

    changes

--- PureScriptSublime.py
+++ PureScriptSublime.py
@@ -4,44 +4,59 @@ import os
 import json
 import pathlib
 
-
 class PureFormatCommand(sublime_plugin.TextCommand):
   def run(self, edit):
 
     region = sublime.Region(0, self.view.size())
     content = self.view.substr(region)
     fname = self.view.file_name()
-    current = pathlib.Path(__file__).parent.resolve()
-    print("current = %s" % current)
+    #current = pathlib.Path(__file__).parent.resolve()
 
-    cmd = ["purty", 
-           "format", 
-           fname] 
+    tmp_file = "/tmp/code.purs"
+    with open(tmp_file, 'w') as f:
+        f.write(content)
+
+    cmd = ["purty",
+           "format",
+           tmp_file] 
     print("cmd = %s" % " ".join(cmd))
+
+    sublime.status_message("Pure format starting..")
+
     stdout, stderr = subprocess.Popen(
       [" ".join(cmd)],
       stdin=subprocess.PIPE,
       stdout=subprocess.PIPE,
-      stderr=subprocess.PIPE,      
+      stderr=subprocess.PIPE,
       shell=True).communicate()
 
-    #if stderr.strip():
-    #  print("GROOVY FORMAT ERROR: %s" % stderr.strip().decode())
-    #  print(stdout.decode('UTF-8'))
-    #else:
-
-    sublime.status_message("Build finished")
-
-    try:
-      r = stdout.decode('UTF-8')
-
-      self.view.replace(edit, region, r)
-    except Exception as e:
-      print("PureScriptFormat fail: %s" % e)
+    if stderr.strip():
       print("PureScriptFormat ERROR: %s" % stderr.strip().decode())
-      print("PureScriptFormat RESULT: %s" % stdout.decode('UTF-8'))
 
-def check_is_enabled_file(file_name):    
+      r = stderr.decode('UTF-8')
+      if 'Error formatting' in r:
+
+        description = r.split("\n")
+        if len(description) > 0:
+          description = description[1].strip()
+        else:
+          description = r
+
+        sublime.error_message("Pure format error: %s" % description)
+
+    else:
+
+
+      try:
+        r = stdout.decode('UTF-8')
+        self.view.replace(edit, region, r)
+        sublime.status_message("Pure format finished")
+      except Exception as e:
+        print("PureScriptFormat fail: %s" % e)
+        print("PureScriptFormat ERROR: %s" % stderr.strip().decode())
+        print("PureScriptFormat RESULT: %s" % stdout.decode('UTF-8'))
+
+def check_is_enabled_file(file_name):
   types = ['.purs']
 
   for t in types:
@@ -51,10 +66,7 @@ def check_is_enabled_file(file_name):
 
 class PureEventDump(sublime_plugin.EventListener):
 
-      
-  def on_post_save(self, view):
+
+  def on_pre_save(self, view):
     if check_is_enabled_file(view.file_name()):
       view.run_command('pure_format')
-
-
-    
